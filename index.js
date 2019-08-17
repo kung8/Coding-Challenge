3d19a9a5014df2761e4e3f3dd5bf78edcc144966
Kevin Ung <ung.kevin78@gmail.com>
2019-08-16 18:34:45 -0600

    problem 1 - fixed missing number requirement

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function sum(num1,num2){
-    if(typeof num1 !== 'number'){
+function sum(num1, num2) {
+    if (typeof num1 !== 'number' || !num1) {
         num1 = 0
-    } else if(typeof num2 !== 'number'){
+    }
+    if (typeof num2 !== 'number' || !num2) {
         num2 = 0
     }
     return num1 + num2
