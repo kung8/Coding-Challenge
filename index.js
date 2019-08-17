f837c55e10b65536ce03dbcfbdd61d2f971ac992
Kevin Ung <ung.kevin78@gmail.com>
2019-08-16 18:45:02 -0600

    problem 1 - corrected the parameter type and accomplished problem 2 as well

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function sum(num1, num2) {
-    if (typeof num1 !== 'number' || !num1) {
-        num1 = 0
-    }
-    if (typeof num2 !== 'number' || !num2) {
-        num2 = 0
-    }
-    return num1 + num2
+function sum(nums) {
+    let arr = nums.split(',')
+    return arr.map(num => {
+        num = +num
+        if (!num) {
+            num = 0
+        }
+        return num
+    }).reduce((total, num) => {
+        return total + num
+    }, 0)
 }
